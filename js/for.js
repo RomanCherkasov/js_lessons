9eecc4c1a9b6e565d1c7fc617b5d6e880cf0c684
Roman Cherkasov <palkamon@yandex.ru>
2021-12-14 08:51:28 +0700

    for and while

---
+++ js/for.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let num = 50;
+
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+
+num = 50;
+do {
+    console.log(num);
+    num++;
+} while (num < 55);
+
+
+num = 50;
+for (let i = 1; i < 8; i++) {
+    if (i == 4){
+        continue;
+    }
+
+    if (i == 7){
+        break;
+    }
+    console.log(`{num} {i}`);
+    num++;
+}
