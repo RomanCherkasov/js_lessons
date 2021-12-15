10e142d97dc23f4b778fc7fa92b577d91f93c351
Roman Cherkasov <palkamon@yandex.ru>
2021-12-15 16:17:24 +0700

    some js brainfuck

---
+++ js/tests.js
@@ -0,0 +1,31 @@
+'use strict';
+let x = 5; console.log(x++);
+
+const a = [];
+console.log(a);
+
+let y = 1;
+let zx = y = 2;
+console.log(y);
+
+let zs = [] + 1 + 2;
+console.log(typeof(zs));
+console.log(zs);
+
+console.log('1'[0]);
+
+const aaa = 2 && 1 && null && 0 && undefined;
+console.log(aaa);
+
+console.log(null || 2 && 3 || 4);
+
+const qwe = [1,2,3];
+const ewq = [1,2,3];
+
+console.log(qwe == ewq);
+
+console.log(+'Infinity');
+console.log('Ёжик' > 'яблоко');
+
+const last = 0 || "" || 2 || undefined || true || falsе ;
+console.log(last);
