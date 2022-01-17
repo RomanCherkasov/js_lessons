c53db5f448ae52ff6fd77a18644b8bb0d8ce6ae9
RomanCherkasov <palkamon@yandex.ru>
2022-01-18 00:01:39 +0700

    getter and setter example

---
+++ js/getterAndSetter.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'alex',
+    age: 25,
+
+    get userAge(){
+        return this.age;
+    },
+    set userAge(num) {
+        this.age = num;
+    }
+};
+console.log(person.userAge = 30);
+console.log(person.userAge);
