f247153ef70ac5071a4ff76a7af049989ec5faa0
RomanCherkasov <palkamon@yandex.ru>
2022-01-17 23:16:11 +0700

    add user informing about wrong input

--- srv_proj/food_prj/js/script.js
+++ srv_proj/food_prj/js/script.js
@@ -450,6 +450,13 @@ window.addEventListener('DOMContentLoaded', () => {
         const input = document.querySelector(selector);
 
         input.addEventListener('input', () => {
+
+            if (input.value.match(/\D/g)) {
+                input.style.border = '2px solid red';
+            } else {
+                input.style.border = 'none';
+            }
+
             switch(input.getAttribute('id')){
                 case 'height':
                     height = +input.value;
