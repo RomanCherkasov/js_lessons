f2363a08c504d7e869374f1a899b3582f5e3d813
Roman Cherkasov <roman@Air-Roman.cn.ru_novotelecom.ru>
2022-02-21 23:47:07 +0700

    some jshint edits

--- js/tryCatch.js
+++ js/tryCatch.js
@@ -1,12 +1,12 @@
 try {
     console.log('Normal');
-    console.log(a)
-    console.log('res')
+    console.log(a);
+    console.log('res');
 } catch(e) {
-    console.log(e.message)
-    console.log('error')
+    console.log(e.message);
+    console.log('error');
 } finally {
-    console.log('run always')
+    console.log('run always');
 }
 
-console.log('Still normal')
+console.log('Still normal');
