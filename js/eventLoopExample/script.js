0ab2cf9ce44ad9fcc506b54f526b2ee9f2c711ab
Roman Cherkasov <palkamon@yandex.ru>
2022-04-03 18:52:41 +0700

    eventloop example

---
+++ js/eventLoopExample/script.js
@@ -0,0 +1,11 @@
+console.log(1);
+
+setTimeout(() => {
+    console.log("First set time out");
+}, 2000);
+
+setTimeout(() => {
+    console.log("Second set time out");
+}, 2000);
+
+console.log(2);
